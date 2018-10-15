b2e623a3a3975a66c00c531daefc66f960f95510
Juan Pablo Ugarte <ugarte@endlessm.com>
2018-10-15 14:18:37 -0700

    Hackyballs: fix regression in drawImageCached() optimization
    Do not use cached image is current transformation is not identity
    
    https://phabricator.endlessm.com/T24014

--- com.endlessm.hackyballs/app/utilities/Optimization.js
+++ com.endlessm.hackyballs/app/utilities/Optimization.js
@@ -3,13 +3,19 @@
 //-------------------------------------------------------
 CanvasRenderingContext2D.prototype.drawImageCached = function (image, dx, dy, dWidth, dHeight)
 {
+    if (!this.getTransform().isIdentity)
+    {
+        this.drawImage(image, dx, dy, dWidth, dHeight);
+        return;
+    }
+
     dWidth = Math.round(dWidth);
     dHeight = Math.round(dHeight);
 
     if (!image._offScreen)
     {
         image._offScreen = document.createElement('canvas');
-        image._offScreenCtx = image._offScreen.getContext('2d')
+        image._offScreenCtx = image._offScreen.getContext('2d');
     }
 
     if (image._offScreen.width !== dWidth || image._offScreen.height !== dHeight)

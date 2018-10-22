bf2260be00fbf7c24616681b25c9295324d3e8c2
Jeffrey Ventrella <ventrella@MacBook-Pro-5.local>
2018-10-22 13:59:44 -0700

    WelcomeApp: added new success image and some logic for fading it in

--- com.endlessm.WelcomeApp/app/Unlock.js
+++ com.endlessm.WelcomeApp/app/Unlock.js
@@ -4,10 +4,10 @@ var canvas = canvasID.getContext( '2d' );
 
 "use strict";
 
-var MODE_FIRST_SCREEN     = 0;
+var MODE_FIRST_SCREEN   = 0;
 var MODE_SOLVING_PUZZLE = 1;
-var MODE_SUCCESS         = 2;
-
+var MODE_SUCCESS        = 2;
+var MODE_FINISHED       = 3;
 
 //---------------------------------------------------------
 // The object globalParameters contains all the data that
@@ -16,16 +16,16 @@ var MODE_SUCCESS         = 2;
 //---------------------------------------------------------
 var globalParameters = 
 {
-    minAmplitude     : 0.0,
-    maxAmplitude     : 0.7,
-    minFrequency     : 0.0,
-    maxFrequency     : 50.0,
-    minPhase         : -3.0,
-    maxPhase         : 3.0,
-    amplitude        : 0.0,
-    frequency        : 0.0,
-    phase            : 0.0,
-    mode             : 0,
+    minAmplitude    : 0.0,
+    maxAmplitude    : 0.7,
+    minFrequency    : 0.0,
+    maxFrequency    : 50.0,
+    minPhase        : -3.0,
+    maxPhase        : 3.0,
+    amplitude       : 0.0,
+    frequency       : 0.0,
+    phase           : 0.0,
+    mode            : 0,
     unlocked        : false
 }
 
@@ -44,38 +44,41 @@ function flip()
 //----------------------
 function Unlock()
 {    
-    var USING_TEST_GUI        = true;
-    var USING_SYNTHESIZER    = true;
+    var USING_TEST_GUI      = false;
+    var USING_SYNTHESIZER   = false;
 
     var IDEAL_AMPLITUDE     = 0.46;
     var IDEAL_FREQUENCY     = 11.84;
-    var IDEAL_PHASE            = -1.536;    
+    var IDEAL_PHASE         = -1.536;    
 
-    var AMPLITUDE_BUFFER     = 0.1;
-    var FREQUENCY_BUFFER     = 0.5;
+    var AMPLITUDE_BUFFER    = 0.1;
+    var FREQUENCY_BUFFER    = 0.5;
     var PHASE_BUFFER        = 0.2;
 
+    var SUCCESS_DURATION        = 30;
     var MILLISECONDS_PER_UPDATE = 30;
-    var SINE_WAVE_RES             = 100;
-    var BASE_NOTE                 = 44;
+    var SINE_WAVE_RES           = 100;
+    var BASE_NOTE               = 44;
     var SINE_WAVE_FREQ_SCALE    = 0.4;
     var SINE_WAVE_Y_POSITION    = canvasID.height * ONE_HALF + 20;
-    var SINE_WAVE_AMP_SCALE        = 300;
-    var FIRST_FLIP_SOUND         = new Audio( "sounds/first-flip.wav" ); 
-    var SUCCESS_SOUND             = new Audio( "sounds/success.wav" ); 
+    var SINE_WAVE_AMP_SCALE     = 300;
+    var FIRST_FLIP_SOUND        = new Audio( "sounds/first-flip.wav" ); 
+    var SUCCESS_SOUND           = new Audio( "sounds/success.wav" ); 
     
-    var _synthesizer             = new Synthesizer();
-    var _background                = new Image();
-    var _testGUI                 = new UnlockTestGUI();
-    var _mousePosition            = new Vector2D();
-    var _prevMousePosition        = new Vector2D();
-    var _mouseVelocity            = new Vector2D();
-    var _vector                    = new Vector2D();
-    var _startTime                = ZERO;
-    var _amplitude                 = ZERO;
-    var _frequency                 = ZERO;
-    var _phase                     = ZERO;
-    var _soundClock                = 0;
+    var _synthesizer            = new Synthesizer();
+    var _background             = new Image();
+    var _glowImage              = new Image();
+    var _testGUI                = new UnlockTestGUI();
+    var _mousePosition          = new Vector2D();
+    var _prevMousePosition      = new Vector2D();
+    var _mouseVelocity          = new Vector2D();
+    var _vector                 = new Vector2D();
+    var _startTime              = ZERO;
+    var _amplitude              = ZERO;
+    var _frequency              = ZERO;
+    var _phase                  = ZERO;
+    var _soundClock             = 0;
+    var _successClock           = 0;
 
     //---------------------------
     this.initialize = function()
@@ -83,10 +86,10 @@ function Unlock()
         //-------------------------------------
         // configure parameters to start
         //-------------------------------------
-        globalParameters.amplitude    = 0.2;
-        globalParameters.frequency    = 20.0;
-        globalParameters.phase        = ZERO;
-        globalParameters.mode         = MODE_FIRST_SCREEN;
+        globalParameters.amplitude  = 0.2;
+        globalParameters.frequency  = 20.0;
+        globalParameters.phase      = ZERO;
+        globalParameters.mode       = MODE_FIRST_SCREEN;
         globalParameters.unlocked   = false;
         
         //---------------------------------------------
@@ -102,9 +105,10 @@ function Unlock()
         _startTime = (new Date).getTime();
 
         //--------------------------------------
-        // load background image
+        // load background and glow images
         //--------------------------------------
         _background.src = "images/first-screen.png";            
+        _glowImage.src  = "images/glow.png";            
         
         //----------------------------
         // apply parameters  
@@ -141,6 +145,22 @@ function Unlock()
         {
             this.updateSolvingPuzzle();
         }
+        else if ( globalParameters.mode == MODE_SUCCESS )
+        {
+            if ( USING_SYNTHESIZER )
+            {
+                _synthesizer.turnOffAllNotes();
+            }
+            
+            _successClock ++;
+            
+            if ( _successClock > SUCCESS_DURATION )
+            {
+                globalParameters.unlocked = true;
+                _background.src = "images/success.png";                
+                globalParameters.mode = MODE_FINISHED;
+            }
+        }
         
         //---------------------------
         // render everything...
@@ -173,31 +193,30 @@ function Unlock()
                 _synthesizer.turnOffAllNotes();
 
                 var note     = BASE_NOTE + _frequency * SINE_WAVE_FREQ_SCALE;
-                var duration = 10.0;
-                var attack   = 0.0;
+                var duration = 2.0;
+                var attack   = 0.1;
                 var release  = 0.0;
 
                 _synthesizer.playNote( note, _amplitude, duration, attack, release );
             }
         }
         
-        var amplitudeDiff     = Math.abs( _amplitude     - IDEAL_AMPLITUDE     );
-        var frequencyDiff     = Math.abs( _frequency     - IDEAL_FREQUENCY     );
-        var phaseDiff         = Math.abs( _phase         - IDEAL_PHASE         );
+        var amplitudeDiff   = Math.abs( _amplitude  - IDEAL_AMPLITUDE   );
+        var frequencyDiff   = Math.abs( _frequency  - IDEAL_FREQUENCY   );
+        var phaseDiff       = Math.abs( _phase      - IDEAL_PHASE       );
 
-        if (( amplitudeDiff < AMPLITUDE_BUFFER     )
-        &&  ( frequencyDiff < FREQUENCY_BUFFER     )
-        &&  ( phaseDiff     < PHASE_BUFFER        ))
+        if (( amplitudeDiff < AMPLITUDE_BUFFER  )
+        &&  ( frequencyDiff < FREQUENCY_BUFFER  )
+        &&  ( phaseDiff     < PHASE_BUFFER      ))
         {
             if ( USING_SYNTHESIZER )
             {            
+                console.log( "OFF NOTES!" );
                 _synthesizer.turnOffAllNotes();
             }
 
             globalParameters.mode = MODE_SUCCESS;
-            globalParameters.unlocked = true;
             SUCCESS_SOUND.play();
-            _background.src = "images/success.png";            
         }
     }
     
@@ -211,6 +230,42 @@ function Unlock()
         //-------------------------------------------
         canvas.drawImage( _background, 0, 0, canvasID.width, canvasID.height );
 
+        //-------------------------------------------
+        // show puzzle solving
+        //-------------------------------------------
+        if (( globalParameters.mode == MODE_SOLVING_PUZZLE )
+        ||  ( globalParameters.mode == MODE_SUCCESS ))        
+        {       
+            //-------------------------------------------
+            // show success effect
+            //-------------------------------------------
+            if ( globalParameters.mode == MODE_SUCCESS )
+            {
+                var f = _successClock / SUCCESS_DURATION;
+                //var wave = ONE_HALF - ONE_HALF * Math.cos( f * PI2 );
+                canvas.globalAlpha = f;        
+                canvas.drawImage( _glowImage, 0, 0, canvasID.width, canvasID.height );        
+                canvas.globalAlpha = 1.0;        
+            }    
+
+            //-------------------------------------------
+            // show composite sine wave
+            //-------------------------------------------
+            canvas.lineWidth = 15;             
+            canvas.strokeStyle = "rgba( 255, 100, 100, 0.3 )";    
+            this.showSineWave();
+
+            canvas.lineWidth = 7;             
+            canvas.strokeStyle = "rgba( 255, 255, 100, 0.3 )";    
+            this.showSineWave();
+
+            canvas.lineWidth = 3;             
+            canvas.strokeStyle = "rgba( 255, 255, 255, 0.3 )";    
+            this.showSineWave();  
+        }
+        
+        
+        /*
         if ( globalParameters.mode == MODE_SOLVING_PUZZLE )
         {                
             //-------------------------------------------
@@ -228,7 +283,16 @@ function Unlock()
             canvas.strokeStyle = "rgba( 255, 255, 255, 0.3 )";    
             this.showSineWave();
         }
-
+        else if ( globalParameters.mode == MODE_SUCCESS )
+        {
+            var f = _successClock / SUCCESS_DURATION;
+            var wave = ONE_HALF - ONE_HALF * Math.cos( f * PI2 );
+            canvas.globalAlpha = wave;        
+            canvas.drawImage( _glowImage, 0, 0, canvasID.width, canvasID.height );        
+            canvas.globalAlpha = 1.0;        
+        }        
+        */
+        
         //-------------------------
         // show user interface
         //-------------------------
@@ -358,13 +422,6 @@ document.onmouseup = function(e)
 {
     var rect = e.target.getBoundingClientRect();
     unlock.mouseUp( e.pageX - rect.left, e.pageY - rect.top );
-}
-
-//-------------------------------
-document.onkeydown = function(e) 
-{
-    if ( e.keyCode === 32 ) { unlock.spaceKeyPressed();  }
-    if ( e.keyCode ===  8 ) { unlock.deleteKeyPressed(); }
 }
 
 

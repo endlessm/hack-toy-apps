c49715254a846b5c720786bd360b9f898d53b15f
Juan Pablo Ugarte <ugarte@endlessm.com>
2019-02-06 14:42:55 -0800

    Gobble: add LevelChooser widget

--- com.endlessm.Gobble/app/startScene.js
+++ com.endlessm.Gobble/app/startScene.js
@@ -24,35 +24,15 @@ class StartScene extends Phaser.Scene {
     }
 
     create (data) {
-        const currentLevel = globalParameters.currentLevel;
-        const levelParams = levelParameters[currentLevel];
+        const levelParams = levelParameters[globalParameters.currentLevel];
         const spacing = 16;
 
         var confetti = this.add.image(0, 0, 'confetti').setOrigin(0,0);
         var logo = this.add.image(0, 0, 'logo').setOrigin(0.5,0.5);
         var controlsText = this.add.text(0, 0, 'Move with', fontConfig);
         var arrowKeys = this.add.image(0, 0, 'arrow-keys');
-        this.startMessage = this.add.text(0, 0, '\n\n', fontConfig).setOrigin(0.5,0.5);
-
-        var prev = new Utils.Button(this, 'prev');
-        var level = this.add.text(0, 0, '', fontConfig);
-        var next = new Utils.Button(this, 'next');
-
-        this.currentLevel = currentLevel;
-        this.updateLevel(prev, next, level);
-
-        const lw = prev.width + spacing + level.width + spacing + next.width + 8;
-        const lh = prev.height + 8;
-        var levelBg = new Utils.TransparentBox(this, lw, lh, 8);
-        var levelBox = new Phaser.GameObjects.Container(this, 0, 0, [levelBg, prev, level, next]);
-
-        /* FIXME: find a better way to align objects */
-        levelBg.setPosition(-lw/2, -lh/2);
-        levelBox.setSize(lw, lh);
-        Phaser.Display.Align.In.LeftCenter(prev, levelBox, -4);
-        Phaser.Display.Align.In.Center(level, levelBox);
-        Phaser.Display.Align.In.RightCenter(next, levelBox, -4);
-
+        this.startMessage = this.add.text(0, 0, levelParams.description, fontConfig).setOrigin(0.5,0.5);
+        this.levelChooser = new LevelChooser(this, 'prev', 'next');
         var startButton = new Utils.Button(this, 'button', 'START');
 
         logo.setScale(0.4);
@@ -70,49 +50,32 @@ class StartScene extends Phaser.Scene {
         Phaser.Display.Align.To.BottomCenter(controlsText, confetti, 0, spacing * 2);
         Phaser.Display.Align.To.BottomCenter(arrowKeys, controlsText, 0, spacing);
         Phaser.Display.Align.To.BottomCenter(this.startMessage, arrowKeys, 0, spacing);
-        Phaser.Display.Align.To.BottomCenter(levelBox, this.startMessage, 0, spacing);
-        Phaser.Display.Align.To.BottomCenter(startButton, levelBox, 0, spacing * 2);
+        Phaser.Display.Align.To.BottomCenter(this.levelChooser, this.startMessage, 0, spacing);
+        Phaser.Display.Align.To.BottomCenter(startButton, this.levelChooser, 0, spacing * 2);
 
         const w = confetti.width;
         const h = startButton.y + startButton.height + spacing;
-
         var bg = new Utils.TransparentBox(this, w, h, 16);
 
         this.add.container(
             (game.config.width - w)/2, (game.config.height - h)/2,
-            [ bg, confetti, logo, controlsText, arrowKeys, this.startMessage, levelBox, startButton ]
+            [ bg, confetti, logo, controlsText, arrowKeys, this.startMessage, this.levelChooser, startButton ]
         );
 
+        this.levelChooser.on('level-changed', (level) => {
+            this.startMessage.setText(levelParameters[level].description);
+        });
+
         startButton.on('pointerup', function () {
-            globalParameters.currentLevel = this.currentLevel;
+            const i = this.levelChooser.currentLevel;
+            globalParameters.currentLevel = i;
             this.scene.get('level').inDemo = false;
-            this.scene.start('level', levelParameters[this.currentLevel]);
-        }, this);
-
-        prev.on('pointerup', function () {
-            this.currentLevel--;
-            this.updateLevel(prev, next, level);
-
-        }, this);
-
-        next.on('pointerup', function () {
-            this.currentLevel++;
-            this.updateLevel(prev, next, level);
+            this.scene.start('level', levelParameters[i]);
         }, this);
 
         /* Start current level */
-        this.scene.launch('level', levelParameters[this.currentLevel]);
+        this.scene.launch('level', levelParams);
         this.scene.get('level').inDemo = true;
-    }
-
-    updateLevel (prev, next, levelText) {
-        /* Update description */
-        this.startMessage.setText(levelParameters[this.currentLevel].description);
-
-        /* Update level text */
-        levelText.setText(`Level ${this.currentLevel + 1}`, fontConfig);
-        prev.sensitive = this.currentLevel > 0;
-        next.sensitive = this.currentLevel < globalParameters.availableLevels-1;
     }
 }
 
---
+++ com.endlessm.Gobble/app/widgets.js
@@ -0,0 +1,60 @@
+/* Phaser UI elements
+ *
+ * Copyright (C) 2019 Endless Mobile, Inc.
+ *
+ * Author: Juan Pablo Ugarte <ugarte@endlessm.com>
+ */
+
+class LevelChooser extends Phaser.GameObjects.Container {
+
+    constructor (scene, prevBtn, nextBtn) {
+        const spacing = 16;
+
+        super(scene, 0, 0);
+
+        var prev = this._prev = new Utils.Button(scene, prevBtn);
+        var level = this._level = new Phaser.GameObjects.Text(scene, 0, 0, '', fontConfig);
+        var next = this._next = new Utils.Button(scene, nextBtn);
+
+        this.currentLevel = globalParameters.currentLevel;
+        this.updateLevel(0);
+
+        const lw = prev.width + spacing + level.width + spacing + next.width + 8;
+        const lh = prev.height + 8;
+        var levelBg = new Utils.TransparentBox(scene, lw, lh, 8);
+
+        this.add([levelBg, prev, level, next]);
+
+        /* FIXME: find a better way to align objects */
+        levelBg.setPosition(-lw/2, -lh/2);
+        this.setSize(lw, lh);
+        Phaser.Display.Align.In.LeftCenter(prev, this, -4);
+        Phaser.Display.Align.In.Center(level, this);
+        Phaser.Display.Align.In.RightCenter(next, this, -4);
+
+        prev.on('pointerup', () => {
+            this.updateLevel(-1);
+        }, this);
+
+        next.on('pointerup', () => {
+            this.updateLevel(1);
+        }, this);
+    }
+
+    updateLevel (increment) {
+        const maxLevel = globalParameters.availableLevels - 1;
+        var currentLevel = this.currentLevel + increment;
+
+        currentLevel = Math.min(Math.max(currentLevel, 0), maxLevel);
+
+        /* Update level text */
+        this._level.setText(`Level ${currentLevel + 1}`, fontConfig);
+        this._prev.sensitive = currentLevel > 0;
+        this._next.sensitive = currentLevel < maxLevel;
+
+        if (this.currentLevel !== currentLevel) {
+            this.currentLevel = currentLevel;
+            this.emit('level-changed', currentLevel);
+        }
+    }
+}

896e4d0f035efe96cdc484bb38c6b408c3eb01de
Juan Pablo Ugarte <ugarte@endlessm.com>
2019-02-07 19:41:30 -0800

    LightSpeed: finish game over dialog first iteration
    Make game over dialog more like the others.
    
    https://phabricator.endlessm.com/T25374

--- com.endlessm.LightSpeed/app/gameOverScene.js
+++ com.endlessm.LightSpeed/app/gameOverScene.js
@@ -17,55 +17,69 @@ class GameOverScene extends Phaser.Scene {
 
     preload () {
         this.load.image('game-over', 'assets/game-over.png');
+        this.load.image('explosion', 'assets/ui/explosion.png');
     }
 
     create (data) {
         const centerX = this.cameras.main.centerX;
         const centerY = this.cameras.main.centerY;
 
-        var box  = this.add.container();
-        /* Astronaut particles */
-        var particles = this.add.particles('particle');
-
-        /* Logo */
         var gameOver = this.add.image(centerX, centerY, 'game-over');
+        gameOver.setScale(0.6);
         this.tweens.add({
             targets: gameOver,
-            scaleX: 0.8,
-            scaleY: 1.6,
+            scaleX: 0.32,
+            scaleY: 0.8,
             duration: 600,
             ease: 'Sine',
             yoyo: true,
             repeat: -1
         });
+
+        var particles = this.add.particles('particle');
         var emitter = particles.createEmitter({
             speed: 128,
             scale: { start: 1, end: 0 },
             blendMode: 'ADD',
-            lifespan: 5000
+            tint: [ 0xffee00, 0xff2900, 0xff8a00, 0xff6600 ],
+            lifespan: 3000
         });
         emitter.startFollow(gameOver);
 
-        /* Bottom text */
-        var text = this.add.text(0, 0, 'Press enter to continue', fontConfig);
-        Phaser.Display.Align.In.BottomCenter(text, gameOver, 0, 256);
-        this.tweens.add({
-            targets: text,
-            alpha: 0.16,
-            duration: 600,
-            ease: 'Sine',
-            yoyo: true,
-            repeat: -1
+        const levelParams = levelParameters[globalParameters.currentLevel];
+        const spacing = 32;
+
+        var pad = this.add.zone(0, 0, 512, 310).setOrigin(0,0);
+        var explosion = this.add.image(0, 0, 'explosion').setOrigin(0,0);
+        this.startMessage = this.add.text(0, 0, levelParams.description, fontConfig).setOrigin(0.5,0.5);
+        this.levelChooser = new LevelChooser(this, 'prev', 'next');
+        var restartButton = new Utils.Button(this, 'button', 'RESTART');
+
+        Phaser.Display.Align.In.Center(explosion, pad);
+        Phaser.Display.Align.In.Center(gameOver, explosion);
+        Phaser.Display.Align.To.BottomCenter(this.startMessage, explosion, 0, spacing);
+        Phaser.Display.Align.To.BottomCenter(this.levelChooser, this.startMessage, 0, spacing);
+        Phaser.Display.Align.To.BottomCenter(restartButton, this.levelChooser, 0, spacing);
+
+        const w = pad.width;
+        const h = restartButton.y + restartButton.height + spacing;
+        var bg = new Utils.TransparentBox(this, w, h, 16);
+
+        this.add.container(
+            (game.config.width - w)/2, (game.config.height - h)/2,
+            [ bg, pad, explosion, particles, gameOver, this.startMessage, this.levelChooser, restartButton ]
+        );
+
+        this.levelChooser.on('level-changed', (level) => {
+            this.startMessage.setText(levelParameters[level].description);
         });
 
-        box.add([particles, gameOver, text]);
-
-        this.input.keyboard.on('keyup', (event) => {
-            if(event.keyCode === Phaser.Input.Keyboard.KeyCodes.ENTER) {
-                globalParameters.playing = true;
-                this.scene.get('level').scene.restart(levelParameters[globalParameters.currentLevel]);
-                this.scene.stop();
-            }
+        restartButton.on('pointerup', function () {
+            const i = this.levelChooser.currentLevel;
+            globalParameters.currentLevel = i;
+            globalParameters.playing = true;
+            this.scene.get('level').scene.restart(levelParameters[i]);
+            this.scene.stop();
         }, this);
     }
 }

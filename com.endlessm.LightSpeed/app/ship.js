endlessm/hack-toy-apps

commit c9203aada19355f49f93ac15af209fd3176771cd

LightSpeed: expose powerup counters and state
Let the quest know how many power up have been spwaned and activated.

https://phabricator.endlessm.com/T26198

com.endlessm.LightSpeed/app/ship.js
@@ -166,6 +166,8 @@ class Ship extends Phaser.Physics.Arcade.Sprite {
     }
 
     shrink(delay) {
+        globalParameters.shrinkUpgradeActivateCountt++;
+
         /* Reset old event */
         if (this._timers.shrink) {
             this._timers.shrink.elapsed = 0;
@@ -194,10 +196,13 @@ class Ship extends Phaser.Physics.Arcade.Sprite {
                 easeParams: [1.4, 0.6],
             });
             this.disableUpgrade();
+            delete this._timers.shrink;
         }, null, this);
     }
 
     increaseAttraction(delay, scale) {
+        globalParameters.attractionUpgradeActivateCountt++;
+
         /* Reset old event */
         if (this._timers.attraction) {
             this._timers.attraction.elapsed = 0;
@@ -217,6 +222,8 @@ class Ship extends Phaser.Physics.Arcade.Sprite {
     }
 
     invulnerable(delay) {
+        globalParameters.invulnerablePowerupActivateCount++;
+
         /* Reset old event */
         if (this._timers.invulnerable) {
             this._timers.invulnerable.elapsed = 0;

com.endlessm.LightSpeed/app/levelScene.js
@@ -8,7 +8,7 @@
 /* exported LevelScene, CONFETTI_COLORS */
 /* global Ship, enemyTypes, saveState, shipTypes, powerupTypes,
     SpawnAstronautScope, SpawnEnemyScope, UpdateEnemyScope, SpawnPowerupScope,
-    ActivatePowerupScope */
+    ActivatePowerupScope, resetGlobalCounters */
 
 const CONFETTI_COLORS = [
     0x1500ff,
@@ -266,14 +266,12 @@ class LevelScene extends Phaser.Scene {
     }
 
     resetQuestData() {
-        /* Reset enemy counters */
+        /* Reset counters */
+        resetGlobalCounters();
+
         this.firstObjectOfType = [];
-        for (let i = 0, n = enemyTypes.length; i < n; i++) {
-            globalParameters[`enemyType${i}SpawnedCount`] = 0;
-            globalParameters[`enemyType${i}MinY`] = +1e9;
-            globalParameters[`enemyType${i}MaxY`] = -1e9;
+        for (let i = 0, n = enemyTypes.length; i < n; i++)
             this.firstObjectOfType[i] = null;
-        }
     }
 
     updatePausedState() {
@@ -623,6 +621,9 @@ class LevelScene extends Phaser.Scene {
                     lifespan: 1200,
                 }).startFollow(obj);
             }
+
+            /* Increment powerup count */
+            globalParameters[`${retval}PowerupSpawnCount`]++;
         }
     }
 
@@ -729,6 +730,8 @@ class LevelScene extends Phaser.Scene {
     }
 
     destroyEnemies() {
+        globalParameters.blowupPowerupActivateCount++;
+
         Sounds.play('lightspeed/powerup/blowup');
 
         /* Iterate over enemy types */
@@ -745,6 +748,9 @@ class LevelScene extends Phaser.Scene {
     }
 
     runActivatePowerup(powerUpType) {
+        /* Increment powerup pickup count */
+        globalParameters[`${powerUpType}PowerupPickedCount`]++;
+
         if (!this.activatePowerup)
             return;
 
@@ -786,7 +792,7 @@ class LevelScene extends Phaser.Scene {
             scaleY: 0.2,
             duration: 500,
             onComplete: () => {
-                this.runActivatePowerup(powerup.texture);
+                this.runActivatePowerup(powerup.texture.key);
                 this.destroySprite(powerup);
             },
         });

com.endlessm.LightSpeed/app/parameters.js
@@ -7,7 +7,7 @@
 
 /* exported enemyTypes, shipTypes, globalParameters, defaultParameters,
     defaultLevelParameters, levelParameters, resetGlobalUserCode,
-    powerupTypes */
+    powerupTypes, upgradeTypes,resetGlobalCounters */
 
 /* Global constants */
 var shipTypes = [
@@ -29,11 +29,16 @@ var powerupTypes = [
     'upgrade',
 ];
 
+var upgradeTypes = [
+    'shrink',
+    'attraction',
+];
 
 /* Freeze constants */
 Object.freeze(shipTypes);
 Object.freeze(enemyTypes);
 Object.freeze(powerupTypes);
+Object.freeze(upgradeTypes);
 
 /*
  * Global parameters exposed to the quests and toolbox
@@ -78,7 +83,7 @@ function resetGlobalUserCode() {
         enemy.position.y = enemy.position.y - 5;
     }`;
 
-    globalParameters.activatePowerupCode = '';
+    globalParameters.activatePowerupCode = ``;
 }
 
 /* We need counters and min/max Y coordinate reached for each enemy type,
@@ -86,15 +91,29 @@ function resetGlobalUserCode() {
  * We also define an update function for each enemy type except for ones which
  * were already defined above.
  *
- * FIXME: can we add support for arrays in clippy!
+ * FIXME: we need support for arrays and object recursion in clippy!
  */
-(function() {
+
+function resetGlobalCounters() {
     enemyTypes.forEach((name, i) => {
         globalParameters[`enemyType${i}SpawnedCount`] = 0;
         globalParameters[`enemyType${i}MinY`] = +1e9;
         globalParameters[`enemyType${i}MaxY`] = -1e9;
     });
 
+    powerupTypes.forEach(name => {
+        globalParameters[`${name}PowerupSpawnCount`] = 0;
+        globalParameters[`${name}PowerupPickedCount`] = 0;
+        globalParameters[`${name}PowerupActivateCount`] = 0;
+    });
+
+    upgradeTypes.forEach(name => {
+        globalParameters[`${name}UpgradeActivateCount`] = 0;
+    });
+}
+
+(function() {
+    resetGlobalCounters();
     resetGlobalUserCode();
 }());
 
@@ -138,7 +157,7 @@ var defaultParameters = {
     }
     return null;`,
 
-    spawnPowerupCode: '',
+    spawnPowerupCode: ``,
 };
 
 /* Per Level defaults:

com.endlessm.LightSpeed/app/userScope.js
@@ -128,7 +128,7 @@ class ActivatePowerupScope extends UserScope {
         };
 
         this._blowUpEnemies = false;
-        this.powerUpType = 0;
+        this.powerUpType = null;
     }
 
     update(data) {

commit b6cdd86d7a720f2fd190d72f85b6c2b97416f7de

LightSpeed: Fix typo in exposed counters
Introduced in c9203aad

https://phabricator.endlessm.com/T26198

com.endlessm.LightSpeed/app/ship.js
@@ -166,7 +166,7 @@ class Ship extends Phaser.Physics.Arcade.Sprite {
     }
 
     shrink(delay) {
-        globalParameters.shrinkUpgradeActivateCountt++;
+        globalParameters.shrinkUpgradeActivateCount++;
 
         /* Reset old event */
         if (this._timers.shrink) {
@@ -201,7 +201,7 @@ class Ship extends Phaser.Physics.Arcade.Sprite {
     }
 
     increaseAttraction(delay, scale) {
-        globalParameters.attractionUpgradeActivateCountt++;
+        globalParameters.attractionUpgradeActivateCount++;
 
         /* Reset old event */
         if (this._timers.attraction) {
81c1dff5f85dc6a919f7f699e924cef2154f9b43
Cosimo Cecchi <cosimo@endlessm.com>
2019-05-06 12:09:48 -0700

    Fizzics: remove old fallback GUI
    We don't use or need this anymore. Remove it to avoid any confusion.

--- com.endlessm.Fizzics/app/HackyBalls.js
+++ com.endlessm.Fizzics/app/HackyBalls.js
@@ -3,7 +3,6 @@ var canvas = canvasID.getContext( '2d' );
 
 "use strict";
 
-var USING_TEST_GUI = false;
 var USING_FALLBACK_SOUNDS = false;
 
 const WIDTH  = 1920;
@@ -19,7 +18,7 @@ var MAX_DEATHEFFECTS = 8
 //---------------------------------------------------------
 // The object globalParameters contains all the data that
 // must be exposed to and share with GTK so that the UI 
-// can be handled externally (not using _hackyBallsGUI).
+// can be handled externally.
 //---------------------------------------------------------
 var globalParameters = 
 {
@@ -284,7 +283,6 @@ function HackyBalls()
     var _balls                  = new Array();
     var _deathAnimations        = new Array();
     var _numBalls               = 0;
-    var _hackyBallsGUI          = new HackyBallsGUI();
     var _leftWall               = ZERO;
     var _topWall                = ZERO;
     var _bottomWall             = HEIGHT;
@@ -439,14 +437,6 @@ function HackyBalls()
         // load images
         //--------------------------------------
         _deleteImage.src = "images/delete-ball.png";
-
-        //---------------------------------------------
-        // initialize user interface with parameters
-        //---------------------------------------------
-        if ( USING_TEST_GUI )
-        {
-            _hackyBallsGUI.initialize( globalParameters );
-        }
 
         //------------------------------------
         // initialize tools
@@ -1070,14 +1060,6 @@ function HackyBalls()
         // show tools
         //-------------------------
         _tools.render();
-        
-        //-------------------------
-        // show user interface
-        //-------------------------
-        if ( USING_TEST_GUI )
-        {
-            _hackyBallsGUI.render();
-        }
 
         //---------------------------------------
         // restore canvas to default transform
@@ -1110,14 +1092,6 @@ function HackyBalls()
 
         x = sx / _worldToWindowScale;
         y = sy / _worldToWindowScale;
-        
-        if ( USING_TEST_GUI )
-        {
-            if ( _grabbedBall == NULL_BALL )
-            {
-                _hackyBallsGUI.setMouseDown( x, y );    
-             }
-        }
         
         //--------------------------
         // update mouse state
@@ -1284,11 +1258,6 @@ function HackyBalls()
         _numBalls = 0;
         _game.setLevel( this, level, _collisionBalls, _ballsWithSomeCollision ); 
         
-        if ( USING_TEST_GUI ) 
-        { 
-            _hackyBallsGUI.setParameters( globalParameters ); 
-        } 
-        
         _flinger.cancel(); 
         _grabbedBall = NULL_BALL;
 
@@ -1342,14 +1311,6 @@ function HackyBalls()
         
         _game.getMouseMoveAction( x, y );
 
-        if ( _grabbedBall == NULL_BALL )
-        {
-            if ( USING_TEST_GUI )
-            {
-                _hackyBallsGUI.setMouseMove( x, y );
-            }
-        }
-        
         this.updateMouse( x, y );
     }
     
@@ -1375,11 +1336,6 @@ function HackyBalls()
 
         x = sx / _worldToWindowScale;
         y = sy / _worldToWindowScale;
-        
-        if ( USING_TEST_GUI )
-        {
-            _hackyBallsGUI.setMouseUp( x, y );
-        }
          
         if ( _flinger.getState() == FLINGER_STATE_PULLING )
         {

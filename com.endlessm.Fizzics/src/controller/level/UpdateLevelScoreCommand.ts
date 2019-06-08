639e2212cbaf302678fe6e2086c24f741e67b552
Ashot Avagyan <avagyanah@gmail.com>
2019-06-08 10:58:59 +0400

    Fizzics: Prevent score to be negative number

--- com.endlessm.Fizzics/src/controller/level/UpdateLevelScoreCommand.ts
+++ com.endlessm.Fizzics/src/controller/level/UpdateLevelScoreCommand.ts
@@ -3,5 +3,5 @@ import { LevelVOProxy } from "../../models/LevelVOProxy";
 export function updateLevelScoreCommand(e: string): void {
   const levelVOProxy = this.retrieveProxy(LevelVOProxy);
   const { score, diamonds, flings } = levelVOProxy.vo;
-  levelVOProxy.updateScore(score + diamonds - flings);
+  levelVOProxy.updateScore(Math.max(0, score + diamonds - flings));
 }
